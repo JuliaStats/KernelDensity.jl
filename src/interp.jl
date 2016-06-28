1fb9daf6e2ec9df4d5db49566426829608760f72
Josh Christie <josh2_@hotmail.com>
2016-06-28 15:53:36 +1000

    Add support for BCfill in InterpKDE

--- src/interp.jl
+++ src/interp.jl
@@ -1,19 +1,29 @@
-type InterpKDE{K,G}
+type InterpKDE{K, G}
     kde::K
     grid::G
 end
 
-function InterpKDE{BC<:BoundaryCondition, IT<:InterpType}(k::UnivariateKDE, bc::Type{BC}=BCnan,it::Type{IT}=InterpQuadratic)
+function InterpKDE{BC<:BoundaryCondition, IT<:InterpType}(k::UnivariateKDE, bc::Type{BC}=BCnan, it::Type{IT}=InterpQuadratic)
     g = CoordInterpGrid(k.x, k.density, bc, it)
-    InterpKDE(k,g)
+    InterpKDE(k, g)
 end
-function InterpKDE{BC<:BoundaryCondition, IT<:InterpType}(k::BivariateKDE, bc::Type{BC}=BCnan,it::Type{IT}=InterpQuadratic)
-    g = CoordInterpGrid((k.x,k.y),k.density,bc,it)
-    InterpKDE{typeof(k),typeof(g)}(k,g)
+function InterpKDE{BC<:BoundaryCondition, IT<:InterpType}(k::BivariateKDE, bc::Type{BC}=BCnan, it::Type{IT}=InterpQuadratic)
+    g = CoordInterpGrid((k.x, k.y), k.density, bc, it)
+    InterpKDE{typeof(k),typeof(g)}(k, g)
+end
+
+# Support BCfill
+function InterpKDE{IT<:InterpType}(k::UnivariateKDE, bc::Number, it::Type{IT}=InterpQuadratic)
+    g = CoordInterpGrid(k.x, k.density, bc, it)
+    InterpKDE(k, g)
+end
+function InterpKDE{IT<:InterpType}(k::BivariateKDE, bc::Number, it::Type{IT}=InterpQuadratic)
+    g = CoordInterpGrid((k.x, k.y), k.density, bc, it)
+    InterpKDE{typeof(k),typeof(g)}(k, g)
 end
 
 
-pdf(ik::InterpKDE,x...) = ik.grid[x...]
+pdf(ik::InterpKDE, x...) = ik.grid[x...]
 
-pdf(k::UnivariateKDE,x) = pdf(InterpKDE(k),x)
-pdf(k::BivariateKDE,x,y) = pdf(InterpKDE(k),x,y)
+pdf(k::UnivariateKDE, x) = pdf(InterpKDE(k), x)
+pdf(k::BivariateKDE, x, y) = pdf(InterpKDE(k), x, y)
--- test/interp.jl
+++ test/interp.jl
@@ -2,11 +2,26 @@ using Base.Test
 using KernelDensity
 using Grid
 
+srand(123)
 X = randn(100)
 Y = randn(100)
 
 k = kde(X)
 @test_approx_eq pdf(k, k.x) k.density
 
-k = kde((X,Y))
+k = kde((X, Y))
 @test_approx_eq pdf(k, k.x, k.y) k.density
+
+# Test BCfill
+test_range = [-20:-10; 10:20]
+test_len = length(test_range)
+
+k = kde(X)
+ik = InterpKDE(k, 0.0, InterpLinear)
+@test_approx_eq pdf(ik, k.x) k.density
+@test pdf(ik, test_range) == zeros(test_len)
+
+k = kde((X, Y))
+ik = InterpKDE(k, 50.0, InterpLinear)
+@test_approx_eq pdf(ik, k.x, k.y) k.density
+@test_approx_eq pdf(ik, test_range, test_range) fill(50.0, test_len, test_len)

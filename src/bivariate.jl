d7524fffdafcd114b4a49c974791b71a23f2e138
Tony Kelman <tony@kelman.net>
2017-03-04 17:51:27 -0800

    lower Compat minimum version (#45)
    * lower Compat minimum version
    
    0.17 needed for new-style abstract type
    
    * remove unneeded compat const
    
    would be needed if parametric (in Compat 0.18), but it isn't here
    
    * remove unneeded at-compat in bivariate.jl
    
    ditto, should squash this

--- src/bivariate.jl
+++ src/bivariate.jl
@@ -85,7 +85,7 @@ function conv(k::BivariateKDE, dist::Tuple{UnivariateDistribution,UnivariateDist
     BivariateKDE(k.x, k.y, dens)
 end
 
-@compat const BivariateDistribution = Union{MultivariateDistribution,Tuple{UnivariateDistribution,UnivariateDistribution}}
+const BivariateDistribution = Union{MultivariateDistribution,Tuple{UnivariateDistribution,UnivariateDistribution}}
 
 function kde(data::Tuple{RealVector, RealVector}, midpoints::Tuple{Range, Range}, dist::BivariateDistribution)
     k = tabulate(data,midpoints)

93437e00dd3c0c070b0934ec5554b83108144381
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2024-01-29 11:27:51 +0100

    Adding help to kde method (#121)
    * Update univariate.jl
    
    * Update src/univariate.jl
    
    Co-authored-by: Tamas K. Papp <tkpapp@gmail.com>
    
    * Update src/univariate.jl
    
    Co-authored-by: Tamas K. Papp <tkpapp@gmail.com>
    
    ---------
    
    Co-authored-by: Tamas K. Papp <tkpapp@gmail.com>

--- src/univariate.jl
+++ src/univariate.jl
@@ -147,6 +147,20 @@ function conv(k::UnivariateKDE, dist::UnivariateDistribution)
 end
 
 # main kde interface methods
+
+"""
+
+	kde(data; kwargs...)
+	kde((xdata, ydata); kwargs...)
+	
+Kernel density estimation method. Returns 1D or 2D KDE object. The grid used and the values of the estimated density can be obtained from fields `.x` and `.density` respectively. To obtain kde values at points different than the initial grid use the `pdf` method.
+
+The keyword arguments are
+* `boundary`: the lower and upper limits of the kde, tuple in 1D case, tuple of tuples in 2D case, 
+* `npoints`: the number of interpolation points to use,
+* `kernel = Normal`: the distributional family from [Distributions.jl](https://github.com/JuliaStats/Distributions.jl),
+* `bandwidth`: the bandwidth of the kernel; default is calculated using Silverman's rule. 
+"""
 function kde(data::AbstractVector{<:Real}, weights::Weights, midpoints::R, dist::UnivariateDistribution) where R<:AbstractRange
     k = tabulate(data, midpoints, weights)
     conv(k,dist)

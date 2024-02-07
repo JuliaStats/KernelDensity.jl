eee2128baa87705113c893fb3d1a4b5360581e08
Jakub Ślęzak <128084860+jaksle@users.noreply.github.com>
2024-02-07 01:39:55 +0100

    Update interp.jl

--- src/interp.jl
+++ src/interp.jl
@@ -26,7 +26,7 @@ InterpKDE(kde::BivariateKDE) = InterpKDE(kde::BivariateKDE, BSpline(Quadratic(Li
 pdf(k::UnivariateKDE,x) = pdf(InterpKDE(k),x)
 pdf(ik::InterpKDE,x::Real...) = ik.itp(x...)
 pdf(ik::InterpKDE,xs::AbstractVector) = [ik.itp(x) for x in xs]
-Base.broadcasted(::typeof(pdf),k::UnivariateKDE,xs) = InterpKDE(k).itp.(xs)
+Base.broadcast(::typeof(pdf),k::UnivariateKDE,xs) = InterpKDE(k).itp.(xs)
 
 pdf(k::BivariateKDE,x,y) = pdf(InterpKDE(k),x,y)
 pdf(ik::InterpKDE,xs::AbstractVector,ys::AbstractVector) = [ik.itp(x,y) for x in xs, y in ys]
